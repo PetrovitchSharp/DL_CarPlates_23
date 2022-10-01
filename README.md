737763c3fc07e42dcd974b539c471fec60d93377
Mikhail Gurevich <gurevich3004@gmail.com>
2022-10-01 10:17:45 +0300

    Add training CLI

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # DL_CarPlates_23
 Car plates recognition command line application
+
+1. Training - train.py
+
+    Training CLI can be used with following parameters:
+
+        -h, --help            show this help message and exit
+        -data DATA            dataset path
+        -output OUTPUT        output path
+        -num_epochs NUM_EPOCHS
+                                train epochs
+        -batch_size BATCH_SIZE
+                                batch size
+        -exp_name EXP_NAME    experiment name
--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 import torch
 import tqdm
@@ -6,73 +8,109 @@ from torchvision import transforms
 from dataset import load_dataset
 from utils import collate_fn, create_model, save_model
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-model = create_model()
+def make_parser():
+    parser = argparse.ArgumentParser(
+        description='Train carplate model'
+        )
+    parser.add_argument('-data', type=str,
+                        default='data',
+                        help='dataset path')
+    parser.add_argument('-output', type=str,
+                        default='output',
+                        help='output path')
+    parser.add_argument('-num_epochs', type=int,
+                        default=10,
+                        help='train epochs')
+    parser.add_argument('-batch_size', type=int,
+                        default=2,
+                        help='batch size')
+    parser.add_argument('-exp_name', type=str,
+                        default='fasterrcnn',
+                        help='experiment name')
+    return parser
 
-transformations = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )
-])
 
-train_dataset = load_dataset(
-    '/kaggle/input/car-plates-ocr/data',
-    transformations,
-    'train')
-val_dataset = load_dataset(
-    '/kaggle/input/car-plates-ocr/data',
-    transformations,
-    'val')
+def main():
+    parser = make_parser()
+    args = parser.parse_args()
+    print(args)
+    data_path = args.data
+    output_path = args.output
+    num_epochs = args.num_epochs
+    batch_size = args.batch_size
+    exp_name = args.exp_name
 
-train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=2, shuffle=True, num_workers=4,
-    collate_fn=collate_fn)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = create_model()
 
-val_loader = torch.utils.data.DataLoader(
-    val_dataset, batch_size=2, shuffle=False, num_workers=4,
-    collate_fn=collate_fn)
+    transformations = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
 
-params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(params, lr=0.005,
-                            momentum=0.9, weight_decay=0.0005)
+    train_dataset = load_dataset(
+        data_path,
+        transformations,
+        'train')
+    val_dataset = load_dataset(
+        data_path,
+        transformations,
+        'val')
 
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                               step_size=3,
-                                               gamma=0.1)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, num_workers=4,
+        collate_fn=collate_fn)
 
-num_epochs = 2
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=False, num_workers=4,
+        collate_fn=collate_fn)
 
-for epoch in range(num_epochs):
-    model.train()
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
 
-    for images, targets in tqdm.tqdm(train_loader):
-        images = list(image.to(device) for image in images)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        loss_dict = model(images, targets)
-        losses = sum(loss for loss in loss_dict.values())
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
 
-        optimizer.zero_grad()
-        losses.backward()
-        optimizer.step()
+    for epoch in range(num_epochs):
+        model.train()
 
-    batch_losses = []
-    for images, targets in tqdm.tqdm(val_loader):
-        images = list(image.to(device) for image in images)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        loss_dict = model(images, targets)
-        losses = sum(loss for loss in loss_dict.values())
-        batch_losses.append(losses.item())
-        optimizer.zero_grad()
+        for images, targets in tqdm.tqdm(train_loader):
+            images = list(image.to(device) for image in images)
+            targets = [{k: v.to(device)
+                       for k, v in t.items()} for t in targets]
+            loss_dict = model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
 
-    batch_losses = np.array(batch_losses)
-    batch_losses = batch_losses[np.isfinite(batch_losses)]
-    print(f'Epoch: {epoch}, Valid_loss: {np.mean(batch_losses)}')
-    lr_scheduler.step()
+            optimizer.zero_grad()
+            losses.backward()
+            optimizer.step()
 
-save_model(model, 'fasterrcnn', '../models')
+        batch_losses = []
+        for images, targets in tqdm.tqdm(val_loader):
+            images = list(image.to(device) for image in images)
+            targets = [{k: v.to(device) for k, v in t.items()}
+                       for t in targets]
+            with torch.no_grad():
+                loss_dict = model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
+            batch_losses.append(losses.item())
 
-print("Training is finished")
+        batch_losses = np.array(batch_losses)
+        batch_losses = batch_losses[np.isfinite(batch_losses)]
+        print(f'Epoch: {epoch}, Valid_loss: {np.mean(batch_losses)}')
+        lr_scheduler.step()
+
+    save_model(model, f'{exp_name}_{num_epochs}.pth', output_path)
+
+    print("Training is finished")
+
+
+if __name__ == '__main__':
+    main()
